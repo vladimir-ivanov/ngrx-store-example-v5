ad987b8554d7bde9730dcf0196479c887faf2d3e
Vladimir Ivanov <vladoivan78@gmail.com>
2018-11-06 22:05:01 +0000

    first commit

---
+++ src/app/reducers/index.ts
@@ -0,0 +1,22 @@
+import {
+  ActionReducer,
+  ActionReducerMap,
+  createFeatureSelector,
+  createSelector,
+  MetaReducer
+} from '@ngrx/store';
+import { environment } from '../../environments/environment';
+import * as fromUser from './user.reducer';
+
+export interface State {
+
+  user: fromUser.State;
+}
+
+export const reducers: ActionReducerMap<State> = {
+
+  user: fromUser.reducer,
+};
+
+
+export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
